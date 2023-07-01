f10ade0c823d514e988535a66de0d37f3b1be870
MosesEjim <mosesejim@outlook.com>
2023-07-01 14:00:58 +0100

    changes php version

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,6 +3,7 @@ echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
+php artisan key:generate
 echo "Caching config..."
 php artisan config:cache
 
