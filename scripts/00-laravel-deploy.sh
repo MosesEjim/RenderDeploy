6d800a6d4cc00cf32f64156a31ecaa507d6b1c1d
MosesEjim <mosesejim@outlook.com>
2023-07-03 00:27:20 +0100

    keygen

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,12 +3,15 @@ echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
+echo "Generating App Key ..."
+php artisan key:generate --show
 
 echo "Caching config..."
 php artisan config:cache
 
 echo "Caching routes..."
 php artisan route:cache
+
 
 echo "Running migrations..."
 php artisan migrate --force
